a41957534a47243d6e723805c8a9c92e7a4e0f64
Alireza Zare <Azare242@gmail.com>
2023-04-07 00:38:13 +0430

    phase 1: add special characters to stopwords

--- p1/util/pre_processor.py
+++ p1/util/pre_processor.py
@@ -7,7 +7,14 @@ class pre_processor:
         self.normalizer = Normalizer()
         self.stemmer = Stemmer()
         self.lemmatizer = Lemmatizer()
-        self.stop_words = stopwords_list()
+        self.stop_words = self.create_stopwords_list()
+
+    def create_stopwords_list(self):
+        li = stopwords_list()
+        special_chars = ['!', '\"', '#', '(', ')', '*', '-', '.', '/', ':', '[', ']', '«', '»', '،', '؛', '؟']
+        for x in special_chars:
+            li.append(x)
+        return li
 
     def is_stopword(self, word):
         return word in self.stop_words
