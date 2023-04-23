0e87c8115bfcbd08d7d78e5283f94bf78b68d80d
Alireza Zare <Azare242@gmail.com>
2023-04-23 15:14:28 +0430

    phase 1: the end

--- p1/main.py
+++ p1/main.py
@@ -1,17 +1,21 @@
 from p1.model.data_set import data_set
 from model.positional_index import positional_index
 from util.query_processor import query_processor
-
+def print_query_result(list_doc_ids: list, DataSet: data_set):
+    for doc_id in list_doc_ids[:5]:
+        DataSet.print_by_doc_id(doc_id)
 if __name__ == '__main__':
-    print('please wait, data is processing...')
     data = data_set()
     PositionalIndex = positional_index(data_set=data)
-    print('processing ends')
-    while True:
-        term = input('>> ')
-        if term == 'exit':
-            break
-        if term in PositionalIndex.index.keys():
-            print(PositionalIndex.index[term].string())
-        else:
-            print('term not found')
+    QueryProcessor = query_processor(pindex=PositionalIndex)
+    Q1 = QueryProcessor.answer('باشگاه های فوتسال آسیا')
+    Q2 = QueryProcessor.answer('باشگاه های فوتسال ! آسیا')
+    Q3 = QueryProcessor.answer('"سهمیه المپیک"')
+    Q4 = QueryProcessor.answer('طلای "لیگ برتر" ! والیبال')
+    Q5 = QueryProcessor.answer('مایکل ! جردن')
+    l = [Q1, Q2, Q3, Q4, Q4]
+    for i in l:
+        print_query_result(i, data)
+        print('---------------------------------------------')
+
+
